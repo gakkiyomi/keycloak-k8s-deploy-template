e1e271252aee2fcc921d9d7c5c1894e326e86f7a
fangcong <fangcong@sky-cloud.net>
2020-12-12 14:50:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # keycloak-k8s-deploy-template
 keycloak-k8s部署脚本
+
+部署前准备
++ docker环境
++ k8s环境
++ postgresql数据库
+
+保证好这三个前提后，修改数据库信息，修改好svc.yaml的宿主机ip后
+直接执行 **deploy.sh**
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!bash/sh
+echo "===========开始安装keycloak========"
+
+docker pull jboss/keycloak:6.0.0
+
+kubectl create ns keycloak || echo "namespace system already exist"
+
+kubectl create -f .
+
+echo "=============安装完毕============="
+
+
+
