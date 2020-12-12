ed940a90a8f93c1c53ded96632facea1bf2d8003
gakkiyomi <41406557+gakkiyomi@users.noreply.github.com>
2020-12-12 14:51:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "===========开始安装keycloak========"
 
 docker pull jboss/keycloak:6.0.0
 
-kubectl create ns keycloak || echo "namespace system already exist"
+kubectl create ns keycloak || echo "namespace keycloak already exist"
 
 kubectl create -f .
 
